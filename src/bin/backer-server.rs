49a27446b082716bc0c4eff5f964b0bf73814f10
duyunzhi <duyunzhi@micstack.com>
2023-01-29 16:32:02 +0800

    added version command

--- src/bin/backer-server.rs
+++ src/bin/backer-server.rs
@@ -10,6 +10,7 @@ use backer::init::init::init;
 use backer::packet::message::{Message, Protocol};
 use backer::packet::tcp_packet::{Handler, TcpHandler, TcpServer};
 use backer::utils::file;
+use backer::version;
 
 #[derive(Parser)]
 struct Opts {
@@ -20,7 +21,18 @@ struct Opts {
     /// files backup dir
     #[clap(short = 'b', long, default_value = "")]
     backup_dir: String,
+
+    /// Display the version
+    #[clap(short, long, action = ArgAction::SetTrue)]
+    version: bool,
 }
+
+const VERSION_INFO: &'static version::VersionInfo = &version::VersionInfo {
+    name: "backer-server",
+    version: "0.1.0",
+    compiler: env!("RUSTC_VERSION"),
+    compile_time: env!("COMPILE_TIME"),
+};
 
 #[cfg(unix)]
 fn wait_on_signals() {
@@ -64,6 +76,12 @@ impl Handler for BackerServerHandle {
 fn main() {
     init();
     let mut opts = Opts::parse();
+
+    if opts.version {
+        println!("{}", VERSION_INFO);
+        return;
+    }
+
     if opts.backup_dir.is_empty() {
         match home::home_dir() {
             Some(path) => {
